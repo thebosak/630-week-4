e87a28384a2990e43f77740ea2e33c1f5d6a0e02
thebosak <33043855+thebosak@users.noreply.github.com>
2019-05-30 21:44:21 -0500

    next commit

--- main/Main.py
+++ main/Main.py
@@ -7,7 +7,12 @@ Created on May 28, 2019
 import sqlite3
 
 connection = sqlite3.connect("C:\\Users\\Brad Bosak\\git\\630-week-4\\database.db")
-connection.execute('drop table if exists People')
+
+#Drop tables if they exists to start over
+connection.execute('DROP TABLE IF EXISTS People')
+connection.execute('DROP TABLE IF EXISTS Experiment')
+
+#Create People table
 connection.execute('''CREATE TABLE People(
     PersonID integer,
     Name text,
@@ -20,6 +25,8 @@ connection.execute('''INSERT INTO People (PersonID, Name, Position, Phone, Locat
 connection.execute('''INSERT INTO People (PersonID, Name, Position, Phone, Location) VALUES ('3','David','Research Assistant','555-123-0004','8')''')
 connection.execute('''INSERT INTO People (PersonID, Name, Position, Phone, Location) VALUES ('4','Edward','Toadie','None','Basement')''')
 connection.execute('''INSERT INTO People (PersonID, Name, Position, Phone, Location) VALUES ('5','Brad','Captain','555-123-0005','Captains Quarters')''')
+
+#Create Experiment table
 connection.execute('''CREATE TABLE Experiment(
     ResearcherID integer,
     Experiment text)''')
@@ -28,19 +35,29 @@ connection.execute('''INSERT INTO Experiment (ResearcherID, Experiment) VALUES (
 connection.execute('''INSERT INTO Experiment (ResearcherID, Experiment) VALUES ('2','XYZ thing')''')
 connection.execute('''INSERT INTO Experiment (ResearcherID, Experiment) VALUES ('3','Andromeda Strain')''')
 connection.execute('''INSERT INTO Experiment (ResearcherID, Experiment) VALUES ('4','Basement disease')''')
-connection.execute('''INSERT INTO Experiment (ResearcherID, Experiment) VALUES ('5','None')''')
+connection.execute('''INSERT INTO Experiment (ResearcherID, Experiment) VALUES ('5','Strange thing')''')
 crsr = connection.cursor()
-#crsr.execute('select * from Experiment')
-crsr.execute('select p.Name, e.Experiment from People as p join Experiment as e where e.ResearcherID == p.PersonID')
+crsr.execute('SELECT P.Name, E.Experiment FROM People AS P JOIN Experiment AS E WHERE E.ResearcherID == P.PersonID')
 rows = crsr.fetchall()
-#for row in rows:
-#    print (row)
+print('Initial researchers and experiments:')
+print()
 for i in rows:
     print ('Name: %s\n\tExperiment: %s' % (i[0],i[1]))
+print()
 
-connection.execute("delete from People where PersonID == '0'")
-crsr.execute('select * from People')
-rows=crsr.fetchall()
-for row in rows:
-    print(row)
+#Add a new user and experiment to database
+connection.execute('''INSERT INTO People (PersonID, Name, Position, Phone, Location) VALUES ('6','Fry','Delivery Boy','555-123-0005','Benders Closet')''')
+
+#Delete Alice from the People table
+connection.execute("DELETE FROM People WHERE PersonID == '0'")
+
+#Reassign Alice's experiment to Fry's ID
+connection.execute("UPDATE Experiment SET ResearcherID = '6' WHERE ResearcherID == '0'")
+
+#Print experiments and researchers
+crsr.execute('SELECT P.Name, E.Experiment FROM People AS P JOIN Experiment AS E where E.ResearcherID == P.PersonID')
+rows = crsr.fetchall()
+print("After reassigning Alice's experiment:")
+for i in rows:
+    print ('Name: %s\n\tExperiment: %s' % (i[0],i[1]))
 connection.close()
